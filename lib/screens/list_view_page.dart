3cad5d46f087db7f1bd67ee490051f21a9a8ec2c
Sharan-ayush08 <ayushsharan22@gmail.com>
2021-08-10 10:56:25 -0700

    not wroking

--- lib/screens/list_view_page.dart
+++ lib/screens/list_view_page.dart
@@ -9,6 +9,8 @@ class ListViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List _itemNames = items!.keys.toList();
+
+    print(_itemNames);
     var _isDetail = false;
     if (items!.entries.first.value.entries.first.value is String) {
       _isDetail = true;
@@ -27,35 +29,47 @@ class ListViewPage extends StatelessWidget {
               itemCount: _itemNames.length,
               itemBuilder: (BuildContext ctx, int index) {
                 return Card(
-                  child: Column(
-                    children: <ListTile>[
-                      ListTile(
-                        onTap: () {
-                          print(_isDetail);
-                          if (_isDetail) {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => ViewDetails(
-                                      title: _itemNames[index],
-                                      details: items![_itemNames[index]],
-                                    )));
-                          } else {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => ListViewPage(
-                                      title: _itemNames[index],
-                                      items: items![_itemNames[index]],
-                                    )));
-                          }
-                        },
-                        title: Text(
-                          _itemNames[index],
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                      )
-                    ],
+                  child: ExpansionTile(
+                    title: Column(
+                      children: <ListTile>[
+                        ListTile(
+                          onTap: () {
+                            print(_isDetail);
+                            if (_isDetail) {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => ViewDetails(
+                                        title: _itemNames[index],
+                                        details: items![_itemNames[index]],
+                                      )));
+                            } else {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => ListViewPage(
+                                        title: _itemNames[index],
+                                        items: items![_itemNames[index]],
+                                      )));
+                            }
+                          },
+                          title: Text(
+                            _itemNames[index],
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        )
+                      ],
+                    ),
+                    children: [Text(_itemNames.toString())],
                   ),
                 );
               },
             ),
     );
   }
+}
+
+class ExpansionItem {
+  bool isExpanded;
+  final String header;
+  final String body;
+
+  ExpansionItem(
+      {required this.header, required this.body, this.isExpanded: false});
 }
--- lib/screens/view_details.dart
+++ lib/screens/view_details.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-class ViewDetails extends StatelessWidget {
+class ViewDetails extends StatefulWidget {
   final title;
   final details;
+
   const ViewDetails({
     Key? key,
     this.title,
@@ -10,71 +11,126 @@ class ViewDetails extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _ViewDetailsState createState() => _ViewDetailsState();
+}
+
+class _ViewDetailsState extends State<ViewDetails> {
+  // List<ExpansionItem> work = <ExpansionItem>[
+  //   ExpansionItem(header: "header", body: "This is the First Header"),
+  //   ExpansionItem(header: "header1", body: "This is the Second Header"),
+  //   ExpansionItem(header: "header2", body: "This is the Third Header"),
+  // ];
+
+  @override
   Widget build(BuildContext context) {
     // final _drugs = Provider.of<Medicines>(context).items;
-    final List<String> _points = details.keys.toList();
+
+    final List<String> _points = widget.details.keys.toList();
+    // final List values = widget.items!.keys.toList();
+    // print(values);
+
+    print(_points);
+    // print(widget);
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      appBar: AppBar(
-        title: Text(title),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.save),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.share),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 5),
-              child: Card(
+      body: ListView.builder(
+          itemCount: _points.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ExpansionTile(
+              title: Container(
                 child: ListTile(
-                  title: Text(
-                    title,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  // subtitle: Text('Orenica, Orenica Clickjet'),
+                  leading: Text(_points[index]),
+                  // trailing: Text(values[index]),
                 ),
               ),
-            ),
-            Card(
-              child: ListTile(
-                  onTap: () {},
-                  title: Text(
-                    "Health Directory",
-                    style: TextStyle(fontSize: 15.0),
-                  ),
-                  subtitle:
-                      Text("Need to make a referral? Serach by specialty."),
-                  trailing: Icon(
-                    Icons.arrow_forward,
-                    size: 15.0,
-                  )),
-            ),
-            Card(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: _points.length,
-                itemBuilder: (BuildContext ctx, int index) {
-                  return ListTile(
-                    onTap: () {},
-                    leading: Text(
-                      _points[index],
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
+      // body: ListView(
+      //   children: [
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: ExpansionPanelList(
+      //         expansionCallback: (int index, bool isExpanded) {
+      //           setState(() {
+
+      //             work[index].isExpanded = !work[index].isExpanded;
+      //           });
+      //         },
+      //         children: work.map((ExpansionItem item) {
+      //           return ExpansionPanel(
+      //               headerBuilder: (BuildContext context, bool isExpanded) {
+      //                 return Container(
+      //                   child: Text(item.header),
+      //                 );
+      //               },
+      //               isExpanded: item.isExpanded,
+      //               body: Container(
+      //                 child: Text(item.body),
+      //               ));
+      //         }).toList(),
+      //       ),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
+
+// class ExpansionItem {
+//   bool isExpanded;
+//   final String header;
+//   final String body;
+
+//   ExpansionItem(
+//       {required this.header, required this.body, this.isExpanded: false});
+// }
+
+
+
+
+  // SingleChildScrollView(
+        //   child: Column(
+        //     children: [
+        //       Padding(
+        //         padding: const EdgeInsets.only(top: 5),
+        //         child: Card(
+        //           child: ListTile(
+        //             title: Text(
+        //               widget.title,
+        //               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        //             ),
+        //             // subtitle: Text('Orenica, Orenica Clickjet'),
+        //           ),
+        //         ),
+        //       ),
+        //       Card(
+        //         child: ListTile(
+        //             onTap: () {},
+        //             title: Text(
+        //               "Health Directory",
+        //               style: TextStyle(fontSize: 15.0),
+        //             ),
+        //             subtitle:
+        //                 Text("Need to make a referral? Serach by specialty."),
+        //             trailing: Icon(
+        //               Icons.arrow_forward,
+        //               size: 15.0,
+        //             )),
+        //       ),
+        //       Card(
+        //         child: ListView.builder(
+        //           shrinkWrap: true,
+        //           itemCount: _points.length,
+        //           itemBuilder: (BuildContext ctx, int index) {
+        //             return ListTile(
+        //               onTap: () {},
+        //               leading: Text(
+        //                 _points[index],
+        //                 style: TextStyle(fontSize: 18.0),
+        //               ),
+        //             );
+        //           },
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
